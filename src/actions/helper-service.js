6497b53d807b5890986ba5149b17e11a19fa3fe4
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-25 09:39:36 +0000

    Update helper-service.js
    Added latest version for codepen.

--- src/actions/helper-service.js
+++ src/actions/helper-service.js
@@ -1,11 +1,35 @@
 class HelperService {
+  hasCorrectBoardOrientation({ ranks, isWhiteTurn }) {
+    if (isWhiteTurn && ranks[0] === 8) return true;
+    if (!isWhiteTurn && ranks[0] === 1) return true;
+    return false;
+  }
+  reverseArray(array) {
+    return array.slice().reverse();
+  }
+  reverseBoard(oldHistory) {
+    const history = oldHistory.slice();
+    const current = history[history.length - 1];   
+    current.squares = this.reverseArray(current.squares);
+    current.files = this.reverseArray(current.files);
+    current.ranks = this.reverseArray(current.ranks);
+    return history;
+  }
   isBetween(num1, num2, middleNumber) {
     if (num1 < middleNumber && middleNumber < num2) return true;
     if (num1 > middleNumber && middleNumber > num2) return true;
     return false;
   }
-  isOnDiagonal() {
-
+  isOnDiagonal(fileIndex, from, to, square) {
+    const fileDiff = Math.abs(fileIndex - from.fileIndex);
+    const rankDiff = Math.abs(square.rank - from.rank);
+    if (!(fileDiff === rankDiff)) return false;
+    if (!this.isBetween(from.rank, to.rank, square.rank)) return false;
+    if (!this.isBetween(from.fileIndex, to.fileIndex, fileIndex)) return false;
+    return true;
+  }
+  isFromSquare({ rank, file }, square) {
+    return rank === square.rank && file === square.file;
   }
 }
 
