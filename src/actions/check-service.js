7943caa4ecf3a829da9d041ddbd82c3569e5a832
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-25 16:18:25 +0000

    Update check-service.js
    Added attack finding method.

--- src/actions/check-service.js
+++ src/actions/check-service.js
@@ -1,14 +1,28 @@
+import _helperService from './helper-service';
+
 class CheckService {
-  discoverSquaresUnderThreat(movementService, squares) {
-    console.log('discover', movementService, squares);
-    /* 1) Loop through squares
-     * 2) If contains:
-     * 3) Use piece attackPatterns from square.
-     *    - This will require canMove/canTake checks for each move.
-     * 4) If a King is target, 'In Check' flag.
-     *    - If that colour King was moved there, 'Cancel Move' flag
-     * 
-     */
+  discoverSquaresUnderThreat(movementService, files, squares) {
+    const attacks = [];
+    for(let i = 0, length = squares.length; i < length; i++) {
+      const square = squares[i];
+      if (!square.contains) continue;
+
+      const fileIndex = files.findIndex(x => x === square.file);
+      const piece = square.contains.props;
+      const attackPattern = _helperService.getPieceAttackPattern(piece.name);
+      for(let j = 0, count = attackPattern.length; j < count; j++) {
+        const move = attackPattern[j];
+        const toSquare = { rank: square.rank + move[0], file: files[fileIndex + move[1]] };
+        if (movementService.canTake(square.contains.props, square, toSquare, files, squares)) {
+          attacks.push({ 
+            name: piece.name,
+            colour: piece.colour,
+            square: toSquare
+          });
+        }
+      }
+    }
+    return attacks;
   }
 }
 
