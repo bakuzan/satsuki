bce1882dcc22a63adad3c530e02e7c56e3c8d301
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-30 15:10:00 +0000

    Update values.js
    Add more values that should be changeable from a single location.

--- src/constants/values.js
+++ src/constants/values.js
@@ -1,8 +1,31 @@
 export const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 export const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
+
+export const colours = {
+  white: 'white',
+  black: 'black'
+};
+
+export const pieces = {
+  pawn: 'pawn',
+  rook: 'rook',
+  knight: 'knight',
+  bishop: 'bishop',
+  queen: 'queen',
+  king: 'king'
+};
+
+export const controls = {
+  autoReverse: {
+    on: 'ON',
+    off: 'OFF'
+  }
+};
+
 export const getPlayerColour = (isWhiteTurn) => {
-  return isWhiteTurn ? 'white' : 'black';
+  return isWhiteTurn ? colours.white : colours.black;
 }
+
 export const getAutoReverseBoard = (autoReverseBoard) => {
-  return autoReverseBoard ? 'ON' : 'OFF';
+  return autoReverseBoard ? controls.autoReverse.on : controls.autoReverse.off;
 }
