990fa292a3889267730e92b4dd5b979f0fa227d7
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-30 15:27:15 +0000

    Update values.js
    Okay. This will be how I do constants...final change. Promise.

--- src/constants/values.js
+++ src/constants/values.js
@@ -1,31 +1,28 @@
-export const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-export const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
-
-export const colours = {
-  white: 'white',
-  black: 'black'
-};
-
-export const pieces = {
-  pawn: 'pawn',
-  rook: 'rook',
-  knight: 'knight',
-  bishop: 'bishop',
-  queen: 'queen',
-  king: 'king'
-};
-
-export const controls = {
-  autoReverse: {
-    on: 'ON',
-    off: 'OFF'
+export const Constants = {
+  files: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+  ranks: [8, 7, 6, 5, 4, 3, 2, 1],
+  colours: {
+    white: 'white',
+    black: 'black'
+  },
+  pieces: {
+    pawn: 'pawn',
+    rook: 'rook',
+    knight: 'knight',
+    bishop: 'bishop',
+    queen: 'queen',
+    king: 'king'
+  },
+  controls: {
+    autoReverse: {
+      on: 'ON',
+      off: 'OFF'
+    }
+  },
+  getPlayerColour: (isWhiteTurn) => {
+    return isWhiteTurn ? this.colours.white : this.colours.black;
+  },
+  getAutoReverseBoard: (autoReverseBoard) => {
+    return autoReverseBoard ? this.controls.autoReverse.on : this.controls.autoReverse.off;
   }
-};
-
-export const getPlayerColour = (isWhiteTurn) => {
-  return isWhiteTurn ? colours.white : colours.black;
-}
-
-export const getAutoReverseBoard = (autoReverseBoard) => {
-  return autoReverseBoard ? controls.autoReverse.on : controls.autoReverse.off;
 }
