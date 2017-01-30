d687c2ad483602874ab72a292b9babde75c718e8
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-30 15:30:56 +0000

    Update piece-service.js
    Redo the new format...

--- src/actions/piece-service.js
+++ src/actions/piece-service.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { COLOURS, PIECES } from '../constants/values';
+import Constants from '../constants/values';
 import Piece from '../components/piece/piece';
 import movementService from './movement-service';
 
@@ -7,15 +7,15 @@ class PieceService {
   getStartingPiece(rank, file) {
     if ([1,2,7,8].indexOf(rank) === -1) return null;
 
-    const colour = rank < 5 ? COLOURS.WHITE : COLOURS.BLACK;
-    if ([2,7].indexOf(rank) > -1) return (<Piece name={PIECES.PAWN} colour={colour}  />);
-    if (['a', 'h'].indexOf(file) > -1) return (<Piece name={PIECES.ROOK} colour={colour} />);
-    if (['b', 'g'].indexOf(file) > -1) return (<Piece name={PIECES.KNIGHT} colour={colour} />);
-    if (['c', 'f'].indexOf(file) > -1) return (<Piece name={PIECES.BISHOP} colour={colour} />);
-    return file === 'd' ? (<Piece name={PIECES.QUEEN} colour={colour} />) : (<Piece name={PIECES.KING} colour={colour} />);
+    const colour = rank < 5 ? Constants.colours.white : Constants.colours.black;
+    if ([2,7].indexOf(rank) > -1) return (<Piece name={Constants.pieces.pawn} colour={colour}  />);
+    if (['a', 'h'].indexOf(file) > -1) return (<Piece name={Constants.pieces.rook} colour={colour} />);
+    if (['b', 'g'].indexOf(file) > -1) return (<Piece name={Constants.pieces.knight} colour={colour} />);
+    if (['c', 'f'].indexOf(file) > -1) return (<Piece name={Constants.pieces.bishop} colour={colour} />);
+    return file === 'd' ? (<Piece name={Constants.pieces.queen} colour={colour} />) : (<Piece name={Constants.pieces.king} colour={colour} />);
   }
   canSelectPiece(isWhiteTurn, colour) {
-    return (isWhiteTurn && colour === COLOURS.WHITE) || (!isWhiteTurn && colour === COLOURS.BLACK);
+    return (isWhiteTurn && colour === Constants.colours.white) || (!isWhiteTurn && colour === Constants.colours.black);
   }
   selectAnotherPiece(isWhiteTurn, currentPiece, newSquare) {
     const contains = newSquare.contains;
@@ -31,17 +31,17 @@ class PieceService {
     const rook = [[0, 1], [0, -1], [1, 0], [-1, 0]];
     const bishop = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
     switch(piece) {
-      case PIECES.PAWN:
+      case Constants.pieces.pawn:
         return [[1,-1],[1,1],[-1,-1],[-1,1]];
-      case PIECES.ROOK:
+      case Constants.pieces.rook:
         return rook
-      case PIECES.KNIGHT:
+      case Constants.pieces.knight:
         return [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[-1,2],[1,-2],[-1,-2]];
-      case PIECES.BISHOP:
+      case Constants.pieces.bishop:
         return bishop;
-      case PIECES.QUEEN:
+      case Constants.pieces.queen:
         return [...bishop, ...rook];
-      case PIECES.KING:
+      case Constants.pieces.king:
         return [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,-1],[-1,1]];
       default:
         return null;
