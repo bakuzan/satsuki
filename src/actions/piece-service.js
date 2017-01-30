69e2573142386a04ddba9dc6940ce37fe4efd17d
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-30 15:20:33 +0000

    Update piece-service.js
    Updated values using Constants.

--- src/actions/piece-service.js
+++ src/actions/piece-service.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { COLOURS, PIECES } from '../constants/values';
 import Piece from '../components/piece/piece';
 import movementService from './movement-service';
 
@@ -6,15 +7,15 @@ class PieceService {
   getStartingPiece(rank, file) {
     if ([1,2,7,8].indexOf(rank) === -1) return null;
 
-    const colour = rank < 5 ? 'white' : 'black';
-    if ([2,7].indexOf(rank) > -1) return (<Piece name="pawn" colour={colour}  />);
-    if (['a', 'h'].indexOf(file) > -1) return (<Piece name="rook" colour={colour} />);
-    if (['b', 'g'].indexOf(file) > -1) return (<Piece name="knight" colour={colour} />);
-    if (['c', 'f'].indexOf(file) > -1) return (<Piece name="bishop" colour={colour} />);
-    return file === 'd' ? (<Piece name="queen" colour={colour} />) : (<Piece name="king" colour={colour} />);
+    const colour = rank < 5 ? COLOURS.WHITE : COLOURS.BLACK;
+    if ([2,7].indexOf(rank) > -1) return (<Piece name={PIECES.PAWN} colour={colour}  />);
+    if (['a', 'h'].indexOf(file) > -1) return (<Piece name={PIECES.ROOK} colour={colour} />);
+    if (['b', 'g'].indexOf(file) > -1) return (<Piece name={PIECES.KNIGHT} colour={colour} />);
+    if (['c', 'f'].indexOf(file) > -1) return (<Piece name={PIECES.BISHOP} colour={colour} />);
+    return file === 'd' ? (<Piece name={PIECES.QUEEN} colour={colour} />) : (<Piece name={PIECES.KING} colour={colour} />);
   }
   canSelectPiece(isWhiteTurn, colour) {
-    return (isWhiteTurn && colour === 'white') || (!isWhiteTurn && colour === 'black');
+    return (isWhiteTurn && colour === COLOURS.WHITE) || (!isWhiteTurn && colour === COLOURS.BLACK);
   }
   selectAnotherPiece(isWhiteTurn, currentPiece, newSquare) {
     const contains = newSquare.contains;
@@ -30,17 +31,17 @@ class PieceService {
     const rook = [[0, 1], [0, -1], [1, 0], [-1, 0]];
     const bishop = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
     switch(piece) {
-      case 'pawn':
+      case PIECES.PAWN:
         return [[1,-1],[1,1],[-1,-1],[-1,1]];
-      case 'rook':
+      case PIECES.ROOK:
         return rook
-      case 'knight':
+      case PIECES.KNIGHT:
         return [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[-1,2],[1,-2],[-1,-2]];
-      case 'bishop':
+      case PIECES.BISHOP:
         return bishop;
-      case 'queen':
+      case PIECES.QUEEN:
         return [...bishop, ...rook];
-      case 'king':
+      case PIECES.KING:
         return [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,-1],[-1,1]];
       default:
         return null;
