ca87758e39a73c414de6f6b35b2a98ddfc594e31
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-09 13:39:06 +0000

    Update rule.js
    Added castle rule to dialog.

--- src/components/rule/rule.js
+++ src/components/rule/rule.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react';
+import RuleOption from '../rule-option/rule-option';
 import Constants from '../../constants/values';
 import './rule.css';
 
+class Rule extends React.Component {
+  constructor() {
+    super();
+    
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+  selectRuleToRender(rule) {
+    switch(rule.name) {
+      case Constants.rules.promotion:
+        return this.renderPromotion();
+      case Constants.rules.castle:
+        return this.renderCastle(rule);
+      default:
+        return (<p>Unhandled rule!</p>);
+    }
+  }
+  handleUserInput(option) {
+    this.props.handleUserInput(option);
+  }
+  renderCastle(rule) {
+    const options = [
+      { text: `${Constants.pieces.king} side`, disabled: !rule.king }, 
+      { text: `${Constants.pieces.queen} side`, disabled: !rule.queen }
+    ].map((item, index) => {
+      return (
+        <RuleOption {...item} handleUserInput={this.handleUserInput} />
+      );  
+    });
+    return { title: 'Did you want to castle:', options };
+  }
+  renderPromotion() {
+    const options = [Constants.pieces.rook, Constants.pieces.knight, Constants.pieces.bishop, Constants.pieces.queen].map((item, index) => {
+      return (
+        <RuleOption text={item} disabled="false"
+                    handleUserInput={this.handleUserInput} />
+      );  
+    });
+    return { title: 'Promote your pawn!', options };
+  }
+  render() {
+    const rule = this.selectRuleToRender(this.props.rule);
+    return (
+      <div id="special-rule" className="column">
+        <h4>{ rule.title }</h4>
+        <ul>
+          { rule.options }
+        </ul>
+      </div>
+    );
+  }
+}
 class Rule extends Component {
   selectRuleToRender(rule) {
     switch(rule.name) {
