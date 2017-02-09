a891b2ccc8cfd5c2f90bd9911ca65a3d882fd517
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-09 13:34:12 +0000

    resolves #2 for the most part. More code to follow
    Updated with codepen, will probably refactor castling code into separate service.

--- src/actions/special-rule-service.js
+++ src/actions/special-rule-service.js
@@ -9,26 +9,88 @@ class SpecialRuleService {
     if (!square) return null;
     return Object.assign({}, { name: Constants.rules.promotion, rank: square.rank, file: square.file });
   }
+  checkSquaresForSafePassage(array, attr, side) {
+    array.forEach(x => {
+        if (['f','g'].indexOf(x[attr]) > 0) side.king = false;
+        if (['b','c','d'].indexOf(x[attr]) > 0) side.queen = false;
+    });
+  }
+  canCastle({ squares, attacks, inCheck }, square, colour) {
+    const targetRank = colour === 'white' ? 1 : 8;
+    const rule = { name: 'CASTLE', king: true, queen: true, rank: targetRank };
+    console.log('can castle: ', square, targetRank);
+    if (square.contains.props.name !== 'king') return null;
+    if (inCheck) return null;
+    if (square.rank !== targetRank || square.file !== 'e') return null;
+    // TODO Neither the king nor the chosen rook has previously moved.
+    const rankSquares = squares.filter(x => x.rank === targetRank && x.contains);
+    this.checkSquaresForSafePassage(rankSquares, 'file', rule);
+    console.log('castling check squares: ', rankSquares, rule);
+    if (!(rule.king || rule.queen)) return null;
+    
+    const rankAttacks = attacks.filter(x => x.attacker.colour !== colour && x.to.rank === targetRank);
+    this.checkSquaresForSafePassage(rankAttacks, 'to.file', rule);
+    console.log('castling check attacks: ', rankAttacks, rule);
+    return (rule.king || rule.queen) ? rule : null;
+  }
   promote(rule, option, history) {
     const index = history.length - 1;
     const current = history[index];
     const squareIndex = current.squares.findIndex(x => x.rank === rule.rank && x.file === rule.file);
-    const updatedHistory = update(history, { [index]: {
-      squares: { [squareIndex]: {
-        contains: {
-          props: {
-            name: { $set: option }
-          }
-        }
-      } }
+    const updatedHistory = update(history, { [index]: { 
+      squares: { [squareIndex]: { 
+        contains: { 
+          props: { 
+            name: { $set: option } 
+          } 
+        } 
+      } } 
     } });
-    const { attacks, inCheck, isMate } = checkService.calculatePossibleAttacks(current.files, updatedHistory[index].squares);
-    return update(updatedHistory, {
+    return this.runAttackChecks(updatedHistory, current, index);
+  }
+  runAttackChecks(history, current, index) {
+    const { attacks, inCheck, isMate } = checkService.calculatePossibleAttacks(current.files, history[index].squares);
+    return update(history, { 
       [index]: {
         attacks : { $set: attacks },
         inCheck : { $set: inCheck },
         winner  : { $set: inCheck && isMate }
-      }
+      } 
+    });
+  }
+  updatePieceOnSquare(history, historyIndex, squareIndex, piece) {
+    return update(history, { [historyIndex]: { 
+      squares: { [squareIndex]: { 
+        contains: { $set: piece } 
+      } } 
+    } });
+  }
+  performCastling(history, index, rule, targetFiles) {
+    const current = history[index];
+    for(let i = 0; i < 2; i++) {
+      const fromIndex = current.squares.findIndex(x => x.rank === rule.rank && x.file === targetFiles[i].from);
+      const piece = current.squares[fromIndex].contains;
+      const toIndex = current.squares.findIndex(x => x.rank === rule.rank && x.file === targetFiles[i].to);
+      history = this.updatePieceOnSquare(history, index, toIndex, piece);
+      history = this.updatePieceOnSquare(history, index, fromIndex, null);
+    }
+    return history;
+  }
+  castle(rule, option, history) {
+    const index = history.length;
+    const current = history[history.length - 1];
+    let updatedHistory = update(history, { $push: [current] });
+    const isKingSide = option === 'king side';
+    const castling = [{ from: 'e', to: isKingSide ? 'g' : 'c' }];
+    const direction = isKingSide ? { from: 'h', to: 'f' } : { from: 'a', to: 'd' };
+    castling.push(direction);
+    updatedHistory = this.performCastling(updatedHistory, index, rule, castling);
+    updatedHistory = this.runAttackChecks(updatedHistory, current, index);
+    return update(updatedHistory, { 
+      [index]: {
+        selected: { $set: null },
+        isWhiteTurn : { $set: !current.isWhiteTurn }
+      } 
     });
   }
   applyRule(rule, option, history) {
@@ -36,6 +98,8 @@ class SpecialRuleService {
     switch(rule.name) {
       case Constants.rules.promotion:
         return this.promote(rule, option, history);
+      case Constants.rules.castle:
+        return this.castle(rule, option, history);
       default:
         return history;
     }
