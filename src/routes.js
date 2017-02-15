2a9569eaf186981e5cd741e64d72784c89f59ae1
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-15 13:48:23 +0000

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import App from './views/app/app';
 import Chess from './components/chess/chess';
 import About from './views/about/about';
+import HowToPlay from './views/how-to-play/how-to-play';
 import PageNotFound from './views/page-not-found/page-not-found';
 import Paths from './constants/paths';
 import {Router, Route, IndexRoute, Redirect, browserHistory} from 'react-router';
@@ -16,6 +17,7 @@ class Routes extends Component {
           <IndexRoute component={Chess} />
 
           <Route path={Paths.about} component={About} />
+          <Route path={Paths.howToPlay} component={HowToPlay} />
         </Route>
 
         <Route path="*" component={PageNotFound} />
