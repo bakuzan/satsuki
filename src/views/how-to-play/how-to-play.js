87ed5e05d833919772fc6375f3b2f3d32aac2aa1
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-15 13:50:29 +0000

    Create how-to-play.js
    View skeleton.

---
+++ src/views/how-to-play/how-to-play.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HowToPlay extends Component {
+  render() {
+    return (<div>This page is under construction.</div>);
+  }
+}
+
+export default HowToPlay;
