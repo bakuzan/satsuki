f73f28066cbc011a212fb54c46169923eb09ed67
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-16 14:52:46 +0000

    Create how-to-play.js
    Starting point for menu structure.

---
+++ src/constants/views/how-to-play.js
@@ -0,0 +1,42 @@
+const HowToPlayValues = {
+  menu: [
+      { 
+        id: 'introduction', 
+        title: 'Introduction', 
+        children: []
+      },
+      {
+        id: 'setup', 
+        title: 'Setup', 
+        children: []
+      },
+      {
+        id: 'movement', 
+        title: 'Movement', 
+        children: [
+          { id: 'king', title: 'King' },
+          { id: 'queen', title: 'Queen' },
+          { id: 'bishop', title: 'Bishop' },
+          { id: 'knight', title: 'Knight' },
+          { id: 'rook', title: 'Rook' },
+          { id: 'pawn', title: 'Pawn' }
+        ]
+      },
+      {
+        id: 'special-rules',
+        title: 'Special rules',
+        children: [
+          { id: 'castling', title: 'Castling' },
+          { id: 'en-passant', title: 'En passant' },
+          { id: 'promotion', title: 'Promotion' }
+        ]
+      },
+      {
+        id: 'check',
+        title: 'Check',
+        children: []
+      }
+    ];
+};
+
+export default HowToPlayValues;
