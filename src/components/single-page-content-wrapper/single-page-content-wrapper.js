6836a11232e7a71497ebb0eb69d5d79f3c203893
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-18 10:28:20 +0000

    improved single-page component

--- src/components/single-page-content-wrapper/single-page-content-wrapper.js
+++ src/components/single-page-content-wrapper/single-page-content-wrapper.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import SinglePageNav from '../single-page-nav/single-page-nav';
 import './single-page-content-wrapper.css';
 
 class SinglePageContentWrapper extends Component {
   render() {
     return (
       <section id="single-page-content">
+        {
+          !!this.props.title &&
+          <header>
+            <h1>{ this.props.title }</h1>
+          </header>
+        }
+        <SinglePageNav items={this.props.navItems} />
         { this.props.children }
       </section>
     );
--- src/views/how-to-play/how-to-play.js
+++ src/views/how-to-play/how-to-play.js
@@ -1,89 +1,86 @@
 import React, { Component } from 'react';
-import SinglePageNav from '../../components/single-page-nav/single-page-nav';
 import SinglePageContentWrapper from '../../components/single-page-content-wrapper/single-page-content-wrapper';
 import HowToPlayValues from '../../constants/views/how-to-play';
 
 class HowToPlay extends Component {
   render() {
     return (
-      <div>
-        <SinglePageNav items={HowToPlayValues.menu} />
-        <SinglePageContentWrapper>
-          <section id="introduction">
-            <header>
-              <h2>Introduction</h2>
-            </header>
-            <div>
-            
-            </div>
-          </section>
-          <section id="setup">
-            <header>
-              <h2>Setup</h2>
-            </header>
-            <div>
-            
-            </div>
-          </section>
-          <section id="movement">
-            <header>
-              <h2>Movement</h2>
-            </header>
-            <div>
-              <section id="king">
-                <header>
-                  <h4>King</h4>
-                </header>
-                <div>
-                </div>
-              </section>
-              <section id="queen">
-                <header>
-                  <h4>Queen</h4>
-                </header>
-                <div>
-                </div>
-              </section>
-              <section id="bishop">
-                <header>
-                  <h4>Bishop</h4>
-                </header>
-                <div>
-                </div>
-              </section>
-              <section id="knight">
-                <header>
-                  <h4>Knight</h4>
-                </header>
-                <div>
-                </div>
-              </section>
-              <section id="rook">
-                <header>
-                  <h4>rook</h4>
-                </header>
-                <div>
-                </div>
-              </section>
-              <section id="pawn">
-                <header>
-                  <h4>Pawn</h4>
-                </header>
-                <div>
-                </div>
-              </section>
-            </div>
-          </section>
-          <section id="special-rules">
-            <header>
-              <h2>Special rules</h2>
-            </header>
-            <div>
-            
-            </div>
-          </section>
-        </SinglePageContentWrapper>
-      </div>
+      <SinglePageContentWrapper title="How to play"
+                                navItems={HowToPlayValues.menu}>
+        <section id="introduction">
+          <header>
+            <h2>Introduction</h2>
+          </header>
+          <div>
+
+          </div>
+        </section>
+        <section id="setup">
+          <header>
+            <h2>Setup</h2>
+          </header>
+          <div>
+
+          </div>
+        </section>
+        <section id="movement">
+          <header>
+            <h2>Movement</h2>
+          </header>
+          <div>
+            <section id="king">
+              <header>
+                <h4>King</h4>
+              </header>
+              <div>
+              </div>
+            </section>
+            <section id="queen">
+              <header>
+                <h4>Queen</h4>
+              </header>
+              <div>
+              </div>
+            </section>
+            <section id="bishop">
+              <header>
+                <h4>Bishop</h4>
+              </header>
+              <div>
+              </div>
+            </section>
+            <section id="knight">
+              <header>
+                <h4>Knight</h4>
+              </header>
+              <div>
+              </div>
+            </section>
+            <section id="rook">
+              <header>
+                <h4>rook</h4>
+              </header>
+              <div>
+              </div>
+            </section>
+            <section id="pawn">
+              <header>
+                <h4>Pawn</h4>
+              </header>
+              <div>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section id="special-rules">
+          <header>
+            <h2>Special rules</h2>
+          </header>
+          <div>
+
+          </div>
+        </section>
+      </SinglePageContentWrapper>
     );
   }
 }
